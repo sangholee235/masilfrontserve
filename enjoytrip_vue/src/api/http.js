503010dc074f34512f44bedf8621174651f4fa3d
LSH <tkdgh1135@naver.com>
2024-11-23 22:14:53 +0900

    kakao

--- enjoytrip_vue/src/api/http.js
+++ enjoytrip_vue/src/api/http.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // axios 객체 생성
 export default axios.create({
   // TODO: 요청 기본 URL 설정하기
-  baseURL: "http://localhost:80",
+  baseURL: process.env.BASE_URL,
   // baseURL: "http://192.168.130.93:80",
   headers: {
     "Content-Type": "application/json;charset=utf-8",
