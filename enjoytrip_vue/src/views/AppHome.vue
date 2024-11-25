754a7b665b3620e5991227e83c405e8318aab2cd
LSH <tkdgh1135@naver.com>
2024-11-25 10:16:48 +0900

    feat : loginpage

--- enjoytrip_vue/src/views/AppHome.vue
+++ enjoytrip_vue/src/views/AppHome.vue
@@ -6,11 +6,13 @@
       <h3 style="color: gray">이 동네 저 동네 싸 돌아다니는 행위</h3>
     </div>
 
-    <!-- 오른쪽 영역 (로그인 폼) -->
+    <!-- 오른쪽 영역 -->
     <div class="right-side">
-      <!-- 로그인 폼을 이곳에 삽입 -->
       <b-container class="d-flex justify-content-center">
-        <div class="login-wrapper">
+        <div v-if="isLogin" class="login-wrapper">
+          <h1 class="text-center">이미 로그인을 함</h1>
+        </div>
+        <div v-else class="login-wrapper">
           <h1 class="login-title text-center">M A S I L</h1>
           <b-card class="p-4 login-card">
             <!-- 로그인 실패 시 경고 메시지 -->
