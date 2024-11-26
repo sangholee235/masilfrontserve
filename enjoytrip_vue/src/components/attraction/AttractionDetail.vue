efdd2de9703a04b9cdb24b2eddbb46e638ee4f02
LSH <tkdgh1135@naver.com>
2024-11-26 17:38:33 +0900

    2

--- enjoytrip_vue/src/components/attraction/AttractionDetail.vue
+++ enjoytrip_vue/src/components/attraction/AttractionDetail.vue
@@ -52,7 +52,7 @@ export default {
     };
   },
   computed: {
-    ...mapState("attractionStore", ["attaraction"]),
+    ...mapState("attractionStore", ["attraction"]),
   },
   methods: {
     init() {
