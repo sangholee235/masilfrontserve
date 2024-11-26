027aa9b504a5a084120d7c19162062cd2d91eafe
LSH <tkdgh1135@naver.com>
2024-11-26 09:14:06 +0900

    fix : url

--- enjoytrip_vue/src/api/http.js
+++ enjoytrip_vue/src/api/http.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // axios 객체 생성
 export default axios.create({
   // TODO: 요청 기본 URL 설정하기
-  baseURL: "http://3.39.229.199:80",
+  baseURL: "http://43.203.210.15:80",
   // baseURL: "http://192.168.130.93:80",
   headers: {
     "Content-Type": "application/json;charset=utf-8",
